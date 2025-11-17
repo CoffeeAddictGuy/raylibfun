133b053198181b7186b70cf581d9feafc416f9b3
Richard <richcrwo@gmail.com>
2025-11-17 16:09:04 +0300

    [ADD] Placer test
    Just add function to place diff color to diff places.
    Color pick from bottom inventory
    Add color by left mouse button
    Delete color by right mouse button
    Hold and move mouse to multiply ADD\DEL color

--- games/placer.c
+++ games/placer.c
@@ -1,25 +1,30 @@
 #include "raylib.h"
 #include "raymath.h"
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 
 #define BLOCKS_PER_LINE 20
 #define BLOCKS_COL 5
+#define INVENTORY_ITEMS 4
 
 const int WIDTH = 800;
 const int HEIGHT = 450;
 
 typedef struct Places {
   Vector2 position;
+  int type;
   bool active;
 } Places;
 
 typedef struct Inventory {
   Vector2 position;
-};
-
-typedef struct mType {
   int type;
-} mType;
+} Inventory;
+
+typedef struct Player {
+  int mType;
+} Player;
 
 static void initGame();
 static void drawGame();
@@ -28,8 +33,10 @@ static void updateGame();
 static void duGame();
 
 static Places place[BLOCKS_COL][BLOCKS_PER_LINE] = {0};
+static Inventory inventory[INVENTORY_ITEMS][1] = {0};
 static Vector2 placeSize = {0};
-static mType type = {0};
+static Vector2 invSize = {0};
+static Player plr = {0};
 
 int main() {
   SetTraceLogLevel(LOG_DEBUG);
@@ -42,14 +49,27 @@ int main() {
 }
 
 void initGame() {
-  placeSize = (Vector2){GetScreenWidth() / BLOCKS_PER_LINE, 40};
+  placeSize = (Vector2){(float)GetScreenWidth() / BLOCKS_PER_LINE, 40};
+  invSize = (Vector2){300, 200};
+
+  // map
   for (int i = 0; i < BLOCKS_COL; i++) {
     for (int j = 0; j < BLOCKS_PER_LINE; j++) {
       place[i][j].position =
           (Vector2){j * placeSize.x + 10, i * placeSize.y + 50};
       place[i][j].active = false;
+      place[i][j].type = 0;
     }
   }
+
+  // inventory
+  for (int i = 0; i < INVENTORY_ITEMS; i++) {
+    inventory[i][0].position = (Vector2){i * (invSize.x / 2 + 10) + 70, 300};
+    inventory[i][0].type = i + 1;
+  }
+
+  // player
+  plr.mType = -1;
 }
 
 void drawGame() {
@@ -60,9 +80,25 @@ void drawGame() {
     for (int j = 0; j < BLOCKS_PER_LINE; j++) {
       DrawRectangleLines(place[i][j].position.x, place[i][j].position.y,
                          placeSize.x / 2, placeSize.y / 2, BLACK);
-      if (place[i][j].active) {
+      if (place[i][j].active && place[i][j].type != 0) {
+        DrawRectangle(place[i][j].position.x, place[i][j].position.y,
+                      placeSize.x / 2, placeSize.y / 2, GRAY);
+      }
+      if (place[i][j].active && place[i][j].type == 1) {
+        DrawRectangle(place[i][j].position.x, place[i][j].position.y,
+                      placeSize.x / 2, placeSize.y / 2, RED);
+      }
+      if (place[i][j].active && place[i][j].type == 2) {
         DrawRectangle(place[i][j].position.x, place[i][j].position.y,
                       placeSize.x / 2, placeSize.y / 2, GREEN);
+      }
+      if (place[i][j].active && place[i][j].type == 3) {
+        DrawRectangle(place[i][j].position.x, place[i][j].position.y,
+                      placeSize.x / 2, placeSize.y / 2, YELLOW);
+      }
+      if (place[i][j].active && place[i][j].type == 4) {
+        DrawRectangle(place[i][j].position.x, place[i][j].position.y,
+                      placeSize.x / 2, placeSize.y / 2, PINK);
       }
     }
   }
@@ -78,29 +114,35 @@ void updateGame() {
           GetMouseX() <= place[i][j].position.x + placeSize.x / 2 &&
           GetMouseY() >= place[i][j].position.y &&
           GetMouseY() <= place[i][j].position.y + placeSize.y / 2) {
-        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && !place[i][j].active)
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && !place[i][j].active) {
           place[i][j].active = true;
-        else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT) && place[i][j].active)
+          place[i][j].type = plr.mType;
+        } else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT) && place[i][j].active)
           place[i][j].active = false;
       }
-    }
-  }
-
-  // Inventory
-  for (int i = 0; i < 1; i++) {
-    for (int j = 0; j < 4; j++) {
-      if (GetMouseX() >= (WIDTH / 2 + 100) - (100 * j) &&
-          GetMouseX() <= (WIDTH / 2 + 100) - (100 * j) - 100 &&
-          GetMouseY() >= 300 && GetMouseY() <= 300 + 100)
-        DrawRectangleLines((WIDTH / 2 + 100) - (100 * j), 300, 100, 100, BLACK);
     }
   }
 }
 
 void drawInv() {
-  for (int i = 0; i < 1; i++) {
-    for (int j = 0; j < 4; j++) {
-      DrawRectangleLines((WIDTH / 2 + 100) - (100 * j), 300, 100, 100, BLACK);
+  Color clrSelect = (Color){253, 249, 0, 100};
+  for (int i = 0; i < INVENTORY_ITEMS; i++) {
+    DrawRectangleLines(inventory[i][0].position.x, inventory[0][i].position.y,
+                       invSize.x / 2, invSize.y / 2, BLACK);
+    if (GetMouseX() >= inventory[i][0].position.x &&
+        GetMouseX() <= inventory[i][0].position.x + invSize.x / 2 &&
+        GetMouseY() >= inventory[i][0].position.y &&
+        GetMouseY() <= inventory[i][0].position.y + invSize.y / 2) {
+      DrawRectangle(inventory[i][0].position.x, inventory[0][i].position.y,
+                    invSize.x / 2, invSize.y / 2, YELLOW);
+      if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        TraceLog(LOG_DEBUG, "You pick - %d", inventory[i][0].type);
+        plr.mType = inventory[i][0].type;
+      }
+    }
+    if (plr.mType == inventory[i][0].type) {
+      DrawRectangle(inventory[i][0].position.x, inventory[i][0].position.y,
+                    invSize.x / 2, invSize.y / 2, clrSelect);
     }
   }
 }
